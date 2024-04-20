eb82c2af99e985a782ee39bd77a01b8e04420807
Juanestebane926 <juan.cuello@uao.edu.co>
2024-04-19 22:19:59 -0500

    Api Polizas

--- src/main/java/com/Polizas/Polizas/Services/RedisService.java
+++ src/main/java/com/Polizas/Polizas/Services/RedisService.java
@@ -1,8 +1,31 @@
 package com.Polizas.Polizas.Services;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 @Service
 public class RedisService {
+
+    @Autowired
+    private StringRedisTemplate redisTemplate;
+
+    public void save(Long id,double valor ){
+        LocalDateTime localDateTime = LocalDateTime.now();
+        String fechaHoraActualStr = localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        redisTemplate.opsForList().rightPush(String.valueOf(id),fechaHoraActualStr);
+        redisTemplate.opsForList().rightPush(String.valueOf(id),String.valueOf(valor));
+        redisTemplate.expire(String.valueOf(id),20,java.util.concurrent.TimeUnit.MINUTES);
+    }
+
+    public List<String> getData(Long id){
+        return redisTemplate.opsForList().range(String.valueOf(id),0,-1);
+    }
+
+
 
 }
